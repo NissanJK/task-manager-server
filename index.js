5f3510b33a3f6fc4122760d27e19bd58563420f8
NissanJK <jawadul.karim78@gmail.com>
2025-02-21 19:58:15 +0600

    almost done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://task-manager-a8537.web.app",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     },
 });
@@ -91,7 +91,7 @@ io.on("connection", (socket) => {
     });
 
     socket.on("disconnect", () => {
-        console.log("A user disconnected:", socket.id);
+        //console.log("A user disconnected:", socket.id);
     });
 });
 
