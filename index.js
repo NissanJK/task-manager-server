dce206ff0be310ad5dc07e3f8556bd3aecbc933b
NissanJK <jawadul.karim78@gmail.com>
2025-02-21 20:09:53 +0600

    almost done

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ io.on("connection", (socket) => {
     });
 
     socket.on("disconnect", () => {
-        //console.log("A user disconnected:", socket.id);
+        console.log("A user disconnected:", socket.id);
     });
 });
 
